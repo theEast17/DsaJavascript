79e249bef511b8491e6c6825339f4e3a485db59e
theEast17 <nagarpoorv2001@gmail.com>
2024-03-16 22:37:04 +0530

    DSA with JS

--- pyramid.js
+++ pyramid.js
@@ -1,27 +1,32 @@
 const Pyramid = (n) => {
-    for (let i = 1; i <= n; i++) {
-        let row = '';
-        for (let j = 1; j <= n - i; j++) {
-            row += ' ';
-        }
-        for (let k = 1; k <= 2 * i - 1; k++) {
-            row += '*';
-        }
-        console.log(row);
-    }
+    // 1st
+    // for (let i = 1; i <= n; i++) {
+    //     let row = '';
+    //     for (let j = 1; j <= n - i; j++) {
+    //         row += ' ';
+    //     }
+    //     for (let k = 1; k <= 2 * i - 1; k++) {
+    //         row += '*';
+    //     }
+    //     console.log(row);
+    // }
 
-    for (let i = n; i >= 1; i--) {
-        let row = '';
-        // Add spaces before the stars
-        for (let j = 1; j <= n - i; j++) {
-            row += ' ';
-        }
-        // Add stars
-        for (let k = 1; k <= 2 * i - 1; k++) {
-            row += '*';
-        }
-        console.log(row);
-    }
+    // for (let i = n; i >= 1; i--) {
+    //     let row = '';
+    //     for (let j = 1; j <= n - i; j++) {
+    //         row += ' ';
+    //     }
+    //     for (let k = 1; k <= 2 * i - 1; k++) {
+    //         row += '*';
+    //     }
+    //     console.log(row);
+    // }
+
+    // 2nd
+    for (let i = 1; i <= n; i++) {
+        console.log(' '.repeat(n - i) + '#'.repeat(i * 2 - 1) + ' '.repeat(n - i))
+      }
+
 };
 
 Pyramid(15);
