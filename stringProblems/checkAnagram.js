437ce25682882391297425cd1ae2bc533f5233fa
theEast17 <nagarpoorv2001@gmail.com>
2024-09-13 19:56:54 +0530

    init

--- stringProblems/checkAnagram.js
+++ stringProblems/checkAnagram.js
@@ -12,35 +12,58 @@
 
 // 2nd
 
-const charMap = (str) => {
-    const anagramObj = {}
-    let string = str.toLowerCase().replace(/\W/g, '')
-    for (let i = 0; i < string.length; i++) {
-        if (anagramObj[string[i]]) {
-            anagramObj[string[i]] = anagramObj[string[i]] + 1
-        } else {
-            anagramObj[string[i]] = 1
+// const charMap = (str) => {
+//     const anagramObj = {}
+//     let string = str.toLowerCase().replace(/\W/g, '')
+//     for (let i = 0; i < string.length; i++) {
+//         if (anagramObj[string[i]]) {
+//             anagramObj[string[i]] = anagramObj[string[i]] + 1
+//         } else {
+//             anagramObj[string[i]] = 1
+//         }
+//     }
+//     return anagramObj
+// }
+
+// const checkAnagram = (str1, str2) => {
+//     let string1 = charMap(str1)
+//     let string2 = charMap(str2)
+
+//     // this line of code only for 35 line 
+//     let str1Keys = Object.keys(string1)  
+//     let str2Keys = Object.keys(string2)
+//     if(str1Keys.length !== str2Keys.length){
+//         return false
+//     }
+
+//     for(let key in string1){
+//         if(string1[key]!==string2[key]) return false
+//     }
+//     return true
+
+// }
+
+// console.log(checkAnagram('coding Money', 'money coding'))
+
+
+function checkAnagram(str1,str2){
+    let string1=str1.replace(/[^A-Za-z]/g,'').toLowerCase().split('')
+    let string2=str2.replace(/[^A-Za-z]/g,'').toLowerCase().split('')
+
+
+    for(let i=0;i<string1.length;i++){
+        if(string2.includes(string1[i])){
+            let findindex=string2.indexOf(string1[i])
+            string2.splice(findindex,1)
+        }else{
+            return 'It is not Anagram'
         }
     }
-    return anagramObj
-}
 
-const checkAnagram = (str1, str2) => {
-    let string1 = charMap(str1)
-    let string2 = charMap(str2)
+    if(string2.length===0) return 'Anagram'
 
-    // this line of code only for 35 line 
-    let str1Keys = Object.keys(string1)  
-    let str2Keys = Object.keys(string2)
-    if(str1Keys.length !== str2Keys.length){
-        return false
-    }
-
-    for(let key in string1){
-        if(string1[key]!==string2[key]) return false
-    }
-    return true
 
 }
 
 console.log(checkAnagram('coding Money', 'money coding'))
+
